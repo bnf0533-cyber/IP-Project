ae54b3f25e09f42ac1d9d0c5c6e9b13881217abe
bnf0533-cyber <bnf0533@gmail.com>
2026-02-11 15:05:49 +0200

    add a list Comprehension

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 
 def return_list_of_lists(path):
-    list_of_data = []
     with open(path,"r") as log:
-        for line in log:
-            list_of_data.append(line.strip().split(','))
-    return list_of_data
-data = return_list_of_lists("C:\\Users\\bnf05\\PycharmProjects\\project\\ip project\\network_traffic.log")
-print(data)
+        return [list_of_data.strip().split(",") for list_of_data in log]
+path = "C:\\Users\\bnf05\\PycharmProjects\\project\\ip project\\network_traffic.log"
+x = return_list_of_lists(path)
+
