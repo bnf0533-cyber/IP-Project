2d2e7c9574cfd2bdeaec377ac1e72b27ba907feb
bnf0533-cyber <bnf0533@gmail.com>
2026-02-11 12:30:02 +0200

    fix splitting logic in reader

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def return_list_of_lists(path):
     list_of_data = []
     with open(path,"r") as log:
         for line in log:
-            list_of_data.append(list(line.split()))
-        return list_of_data
+            list_of_data.append(line.strip().split(','))
+    return list_of_data
 data = return_list_of_lists("C:\\Users\\bnf05\\PycharmProjects\\project\\ip project\\network_traffic.log")
+print(data)
