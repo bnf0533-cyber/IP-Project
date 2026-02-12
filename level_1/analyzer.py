588a54046e2e75691e0ea306bcf7bb256fa49137
bnf0533-cyber <bnf0533@gmail.com>
2026-02-12 13:14:50 +0200

    add dict_dark

--- level_1/analyzer.py
+++ level_1/analyzer.py
@@ -1,5 +1,5 @@
 from sys import path_importer_cache
-
+from checks import *
 from reader import *
 def count_requests_by_ip(data):
     all_ip = [row[1] for row in data]
@@ -8,8 +8,16 @@ all_ips = count_requests_by_ip(x)
 
 def map_port_to_protocol(data):
     return {int(row[3]): row[4] for row in data}
-print(map_port_to_protocol(x))
+# print(map_port_to_protocol(x))
 
 def check_night_activity(night_data):
     return [line[0] for line in night_data if line[0].split()[1].startswith(("00","01","02","03","04","05"))]
-print(check_night_activity(x))
+# print(check_night_activity(x))
+
+def identify_suspicions_per_ip(data):
+    ext_ips = get_external_ips(data)
+    sens_ips = [row[1] for row in filter_by_sensitive_ports(data)]
+    unique_ips = set([row[1] for row in data])
+    return {ip: [tag for condition, tag in [(ip in ext_ips, "EXTERNAL_IP"),(ip in sens_ips, "SENSITIVE_PORT")]if condition]for ip in unique_ips}
+print(identify_suspicions_per_ip(x))
+
