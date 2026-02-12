183088ddce600543d7b92ae5316412d165d1701e
bnf0533-cyber <bnf0533@gmail.com>
2026-02-12 12:19:15 +0200

    add fonc

--- level_1/analyzer.py
+++ level_1/analyzer.py
@@ -5,3 +5,7 @@ def count_requests_by_ip(data):
     all_ip = [row[1] for row in data]
     return {ip: all_ip.count(ip) for ip in set(all_ip)}
 all_ips = count_requests_by_ip(x)
+
+def map_port_to_protocol(data):
+    return {int(row[3]): row[4] for row in data}
+print(map_port_to_protocol(x))
